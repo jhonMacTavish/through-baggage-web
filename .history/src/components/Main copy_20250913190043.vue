0b498d789b17d63e8d1d24af88c9f54c2fad6350
john_mactavish <981192661@qq.com>
2025-09-13 19:00:47 +0800

    20250913

--- .history/src/components/Main copy_20250913190043.vue
+++ .history/src/components/Main copy_20250913190043.vue
@@ -2,8 +2,8 @@
  * 2025-09-13 19:00:30
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2025-02-23 20:06:20
- * @LastEditors: john_mactavish 981192661@qq.com
- * @LastEditTime: 2025-09-13 19:00:44
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2025-02-24 12:10:57
  * @FilePath: \through_baggage_webt:\Projects\VS Code\vue-bootstrap-master\src\components\Main.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
